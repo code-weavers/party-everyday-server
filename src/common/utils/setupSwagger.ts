213ce52d3c2d05d5a96a0a73b315ab82266ca3ac
flaviohsprado <flavio.pprado4@gmail.com>
2024-05-27 17:38:08 -0300

    First Commit

--- src/common/utils/setupSwagger.ts
+++ src/common/utils/setupSwagger.ts
@@ -4,13 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 export class SetupSwagger {
    static for(app: INestApplication) {
       const config = new DocumentBuilder()
-         .setTitle('Template API')
+         .setTitle('Party Everyday API')
          .setDescription(
-            'The Template API development with NestJS and using TypeORM for database connection.',
+            'The Party Everyday API is a RESTful API that allows you to manage users, parties, and authentication.',
          )
          .setVersion('1.0')
          .addTag('Authentication')
          .addTag('Users')
+         .addTag('Party')
          .build();
       const document = SwaggerModule.createDocument(app, config);
       SwaggerModule.setup('api', app, document);
